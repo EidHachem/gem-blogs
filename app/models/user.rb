d1fa51f42e3f75829d51772a7672058b42518463
EidHachem <eidhachem1@gmail.com>
2022-08-24 21:08:43 +0300

    Add devise and setup views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-  validates :name, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
--- config/environments.evren/production.rb
+++ config/environments.evren/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "gem_blogs_production"
+  # config.active_job.queue_name_prefix = "my_blog2_production"
 
   config.action_mailer.perform_caching = false
 
